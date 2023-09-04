b88731cb8c732664f671b07d2be85a3c870dd54b
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-09-04 11:01:02 +0300

    gestion des formations par les formateurs

--- backend/routes/formation/formation.js
+++ backend/routes/formation/formation.js
@@ -8,46 +8,12 @@ const Seance = require('../../Modele/formation/Seance');
 const Module = require('../../Modele/formation/Module');
 
 
-
-router.get('/all_demandes_formations', async(req,res) => {
-    Formation.findAll({
-        include: {
-            model: Collaborateur,
-            attributes: ['nom', 'prenom']
-        },
-        where: {
-            approbation: 0 
-        }
-
-    })
-    .then((formation) => {
-        res.status(200).json(
-            formation.map((formation) => {
-                return {
-                    id : formation.id,
-                    theme : formation.theme,
-                    description : formation.description,
-                    duree : formation.duree,
-                    approbation : formation.approbation,
-                    nomformateur: formation.Collaborateur.nom,
-                    prenomformateur: formation.Collaborateur.prenom,
-                }
-            })
-        )
-        console.log(formation)
-    }) 
-})
-
 router.get('/all_formations', async(req,res) => {
     Formation.findAll({
         include: {
             model: Collaborateur,
             attributes: ['nom', 'prenom']
         },
-        where: {
-            approbation: 1 
-        }
-
     })
     .then((formation) => {
         res.status(200).json(
@@ -67,8 +33,8 @@ router.get('/all_formations', async(req,res) => {
     }) 
 })
 
-router.get('/all_formations/:id', async(req,res)=>{
-    const formationId = req.params.id;
+router.get('/all_informations/:idformation', async(req,res)=>{
+    const formationId = req.params.idformation;
         try {
             const modules = await Module.findAll({
                 where:
@@ -91,78 +57,6 @@ router.get('/all_formations/:id', async(req,res)=>{
             res.status(500).json({ message: 'Erreur lors de la récupération des informations de la formation' });
         }
     });
-
-    
-router.post('/approuver/:id', async (req, res) => {
-    const formationId = req.params.id;
-    
-    try {
-        const updatedFormation = await Formation.update(
-            { approbation: 1 },
-            { where: { id: formationId } }
-        );
-
-        if (updatedFormation[0] === 0) {
-            return res.status(404).json({ message: "Formation not found." });
-        }
-
-        return res.status(200).json({ message: "Formation approved successfully." });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "An error occurred while approving the formation." });
-    }
-});
-
-router.post('/annulerapprobation/:id', async (req, res) => {
-    const formationId = req.params.id;
-    try {
-        const updatedFormation = await Formation.update(
-            { approbation: 0 },
-            { where: { id: formationId } }
-        );
-
-        if (updatedFormation[0] === 0) {
-            return res.status(404).json({ message: "Formation not found." });
-        }
-
-        return res.status(200).json({ message: "Formation approved successfully." });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: "An error occurred while approving the formation." });
-    }
-});
-
-router.delete('/desapprouver/:id', async (req, res) => {
-    const { id } = req.params;
-    try {
-        const deleteFormation = await Formation.findByPk(id);
-        if (!deleteFormation) {
-            return res.status(404).json({ error: 'Formation introuvable' });
-        }
-        
-        // Supprimer les séances associées
-        await Seance.destroy({
-            where: {
-                formation: id
-            }
-        });
-
-        // Supprimer les modules associés
-        await Module.destroy({
-            where: {
-                formation: id
-            }
-        });
-
-        // Supprimer la formation elle-même
-        await deleteFormation.destroy();
-
-        res.sendStatus(204);
-    } catch (error) {
-        console.error('Erreur lors de la suppression :', error);
-        res.status(500).json({ message: 'Erreur lors de la suppression' });
-    }
-});
 
 router.get('/formations/:idPersonne',async(req,res)=>{
     const idPersonne = req.params.idPersonne;
