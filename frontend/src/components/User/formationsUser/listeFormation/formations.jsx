15696eb7999b80ff8b23e2093ebf6987b8e84f81
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-09-04 09:01:32 +0300

    formation côté user

--- frontend/src/components/User/formationsUser/listeFormation/formations.jsx
+++ frontend/src/components/User/formationsUser/listeFormation/formations.jsx
@@ -29,7 +29,7 @@ const ListeFormationUser = () => {
     }
     }, [navigate])
     
-    const id = localStorage.getItem('id');
+    // const id = localStorage.getItem('id');
 
   const fetchCollaborateur = () => {
     axios.get('http://localhost:8000/api/formation/all_formations')
@@ -67,10 +67,10 @@ const ListeFormationUser = () => {
                     <div className="search_form">
                       <input type="text"placeholder="Rechercher une formation" value={recherche} onChange={(e)=>{setRecherche(e.target.value)}} className=""></input>
                       <button className="search_Button"> Rechercher </button>
-                      <Link to="#" className="AddCollab_Link">Organiser une formation</Link>
+                      {/* <Link to="#" className="AddCollab_Link">Organiser une formation</Link> */}
                     </div>
                   </div>
-                <Link to={`/mesFormationsUser/${id}`} className="AddCollab_Link">Mes formations</Link>
+                {/* <Link to={`/mesFormationsUser/${id}`} className="AddCollab_Link">Mes formations</Link> */}
               </div>
               {formations.length !== 0 ?(
               <>
