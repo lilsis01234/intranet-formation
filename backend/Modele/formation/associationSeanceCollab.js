e2f3ba731c5732f35ecdb10787943102325ddeca
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-09-06 17:32:14 +0300

    participation de departement à la séance

--- backend/Modele/formation/associationSeanceCollab.js
+++ backend/Modele/formation/associationSeanceCollab.js
@@ -1,13 +1,15 @@
 const Seance = require('./Seance');
 const Collaborateur = require('../Collaborateur');
 const ParticipantsSeance = require('./ParticipantsSeance');
+const Departement = require('../Departement');
 
 Collaborateur.belongsToMany(Seance,{through:ParticipantsSeance,foreignKey:"collaborateur",otherKey:'seance'})
 Seance.belongsToMany(Collaborateur,{through:ParticipantsSeance,foreignKey:"seance",otherKey:'collaborateur'})
-
+Departement.belongsToMany(Seance,{through:ParticipantsSeance,foreignKey:"equipe",otherKey:'equipe'})
 
 module.exports={
     Seance,
     Collaborateur,
-    ParticipantsSeance
+    ParticipantsSeance,
+    Departement
 }
