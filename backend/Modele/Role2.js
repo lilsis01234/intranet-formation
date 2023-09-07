20e8952a9f9108c1b563b5104cbd7aa21930a0d5
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-09-07 15:30:34 +0300

    faire qu'un rôle a plusieurs role2

--- backend/Modele/Role2.js
+++ backend/Modele/Role2.js
@@ -4,33 +4,38 @@ const sequelize = require('../database/database');
 const Role= require ('../Modele/Role');
 // const CompteCollab = require('../Modele/CompteCollab');
 
-class Role2 extends Model{}
+class Role2 extends Model {}
 
-Role2.init({
-    role:{
-        type : DataTypes.INTEGER,
-        allowNull : false,
-        references : {
-            model : Role,
-            key : 'id'
-        },
-        unique : true
+Role2.init(
+  {
+    role: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Role, // Référence au modèle Role
+        key: 'id',
+      },
     },
-    titreRole2 : {
-        type : DataTypes.STRING,
-        allowNull : true,
-        unique : true
-    }
-}, {
+    titreRole2: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false, // Vous pouvez enlever le 'unique: true'
+    },
+  },
+  {
     sequelize,
-    modelName : 'Role2',
+    modelName: 'Role2',
+  }
+);
+
+Role.hasMany(Role2, {
+  foreignKey: 'role', // La clé étrangère dans Role2
+  onUpdate: 'CASCADE',
 });
 
-
 Role2.belongsTo(Role, {
-    foreignKey : 'role',
-    onUpdate : 'CASCADE',
-})
-
+  foreignKey: 'role', // La clé étrangère dans Role2
+  onUpdate: 'CASCADE',
+});
 
 module.exports = Role2;
