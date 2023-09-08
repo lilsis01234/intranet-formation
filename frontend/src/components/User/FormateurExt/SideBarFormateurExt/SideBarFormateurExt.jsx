45c6a3d235b4a19738b1c8c210ecfd1b779b1cd5
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-09-08 15:11:03 +0300

    sidebar formateurs externes

---
+++ frontend/src/components/User/FormateurExt/SideBarFormateurExt/SideBarFormateurExt.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import {HiOutlineHome} from 'react-icons/hi'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {IoMdClose} from 'react-icons/io'
+import { Link } from 'react-router-dom';
+
+const SideBarFormateurExt = () => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const toogleSideBar = () => {
+        setIsSidebarOpen(!isSidebarOpen);
+    }
+    return (
+        <div className="sideBar_container">
+        {isSidebarOpen && (
+            <div className={`sideBar ${isSidebarOpen ? 'open' : 'closed'}`}>
+                <ul className="sideBar_menu">
+                    <li className="sideBar_menu_item " ><Link className="flex flex-row items-center" to="/formateurExt"><HiOutlineHome className="mr-2"/>Acceuil</Link></li>
+                    <li className="sideBar_menu_item " ><Link className="flex flex-row items-center" to="#"><HiOutlineHome className="mr-2"/>Vos formations</Link></li>
+                </ul>
+            </div>
+        )}
+        <button onClick={toogleSideBar} className="sideBar_button">
+                {isSidebarOpen ? <IoMdClose/> : <GiHamburgerMenu />}
+        </button>
+    </div>
+    )
+    }
+export default SideBarFormateurExt
