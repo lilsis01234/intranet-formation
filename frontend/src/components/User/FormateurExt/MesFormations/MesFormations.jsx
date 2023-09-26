5c8043fc7fd74ba8c73bb959fa98bb44f0691bbd
lilsis01234 <tianantenaina.raharimanana@esti.mg>
2023-09-26 16:15:48 +0300

    optimisation de code

--- frontend/src/components/User/FormateurExt/MesFormations/MesFormations.jsx
+++ frontend/src/components/User/FormateurExt/MesFormations/MesFormations.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 import axios from 'axios';
 import { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import SideBarFormateurExt from '../SideBarFormateurExt/SideBarFormateurExt';
-import NavBarFormateurExt from '../NavBarFormateurExt/NavBarFormateurExt'
+
 
 const MesFormations = () => {
     const[Sugg, setListSuggestion]= useState([]);
